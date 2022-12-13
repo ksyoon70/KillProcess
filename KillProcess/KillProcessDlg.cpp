105f5d5a4f73bb294c974895b30ae990e888c52c
윤경섭 <ksyoon70@gmail.com>
2022-12-13 15:08:26 +0900

    [수정 M012]  - 로그 수정

--- KillProcess/KillProcessDlg.cpp
+++ KillProcess/KillProcessDlg.cpp
@@ -2220,7 +2220,7 @@ void CKillProcessDlg::OnAppExit(void)
 {
 	try{
 		m_myTray.DelTrayIcon(GetSafeHwnd());
-		LSITS_Write_ProgramLogFile(_T("Program closed \r\n"));
+		LSITS_Write_ProgramLogFile(_T("OnAppExit Program closed \r\n"));
 	}
 	catch (CMemoryException* e)
 	{
@@ -2475,7 +2475,7 @@ void CKillProcessDlg::OnClose()
 	// TODO: 여기에 메시지 처리기 코드를 추가 및/또는 기본값을 호출합니다.
 	try{
 		m_myTray.DelTrayIcon(GetSafeHwnd());
-		LSITS_Write_ProgramLogFile(_T("Program closed \r\n"));
+		LSITS_Write_ProgramLogFile(_T("OnClose Program closed \r\n"));
 	}
 	catch (CMemoryException* e)
 	{
